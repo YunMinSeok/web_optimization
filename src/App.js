3cc9c5092847c6729976e4054169b606d9491b86
윤민석 <ykmvm147@naver.com>
2023-01-17 19:27:38 +0900

    feat: lazyWithPreload 함수 추가 완료

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import Header from "./components/Header";
 import InfoTable from "./components/InfoTable";
 import SurveyChart from "./components/SurveyChart";
 import Footer from "./components/Footer";
-// import ImageModal from "./components/ImageModal";
 
-const LazyImageModal = lazy(() => import("./components/ImageModal"));
+function lazyWithPreload(importFunction) {
+  const Component = React.lazy(importFunction);
+  Component.preload = importFunction;
+  return Component;
+}
+
+const LazyImageModal = lazyWithPreload(() => import("./components/ImageModal"));
 
 function App() {
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    const component = import("./components/ImageModal");
+    LazyImageModal.preload();
   }, []);
-
-  //   const handleMouseEnter = () => {
-  //     const component = import("./components/ImageModal");
-  //   };
 
   return (
     <div className="App">
@@ -27,7 +28,6 @@ function App() {
         onClick={() => {
           setShowModal(true);
         }}
-        // onMouseEnter={handleMouseEnter}
       >
         올림픽 사진 보기
       </ButtonModal>
